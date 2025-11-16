d8095ba5c0cce95241b5fcc9919c1dde96a86809
jweboy <jweboy0630@gmail.com>
2025-11-17 01:10:41 +0800

    fix: ts 定义报错

--- app/category/[documentId]/commodity/layout.tsx
+++ app/category/[documentId]/commodity/layout.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/navbar";
 import React from "react";
 
-const CommodityLayout = ({ children }: { children: React.ReactElement }) => {
+const CommodityLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar />
